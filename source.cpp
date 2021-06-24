c0255a5ddb803e2f99d8db8fe311af1aa5539906
xo1337 <68725291+xo1337@users.noreply.github.com>
2021-06-23 21:21:19 -0600

    Create source.cpp

---
+++ source.cpp
@@ -0,0 +1,43 @@
+#include <Windows.h>
+#include <detours.h>
+#pragma comment(lib, "detours.lib")
+
+typedef BOOL(APIENTRY* ProtoType_WriteProcessMemory)
+(   
+    HANDLE hProcess,
+    LPVOID lpBaseAddress,
+    LPCVOID lpBuffer,
+    SIZE_T nSize,
+    SIZE_T* lpNumberOfBytesWritten
+ );
+
+ProtoType_WriteProcessMemory WriteProcessMemoryHook = 
+(ProtoType_WriteProcessMemory)((uintptr_t)GetProcAddress(GetModuleHandleA("kernel32.dll"), "WriteProcessMemory"));
+
+void APIENTRY WriteProcessMemoryHooked
+(
+    _In_   HANDLE hProcess,               // Handle To Specified Process
+    _In_   LPVOID lpBaseAddress,          // Dll's memory allocated in a buffer or programs memory.
+    _In_   LPCVOID lpBuffer ,             // Dll Path (Buffer, text)
+    _In_   SIZE_T nSize,                  // Dll Path (Length)
+    _Out_  SIZE_T* lpNumberOfBytesWritten  
+)
+{
+    MessageBoxA(0, "Hooked on WriteProcessMemory!", "Success", 0);
+    const char* dllPath = reinterpret_cast<const char*>(*lpBuffer);
+    MessageBoXA(0, dllPath, "Grabbed Dll Path!",0);
+}
+
+
+BOOL APIENTRY DllMain( HMODULE hModule, DWORD Reason,LPVOID lpReserved)
+{
+    if (Reason == DLL_PROCESS_ATTACH)
+    {
+        DetourTransactionBegin();
+        DetourUpdateThread(GetCurrentThread());
+        DetourAttach(&(LPVOID&)WriteProcessMemoryHook, (PBYTE)WriteProcessMemoryHooked);
+        DetourTransactionCommit();
+    }
+    return TRUE;
+}
+
